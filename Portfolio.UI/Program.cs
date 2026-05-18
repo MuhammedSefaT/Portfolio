41d40d958b9fa20e9abe8ad6db643fc701fcd4e7
MuhammedSefaT <tayazmuhammedsefa@gmail.com>
2026-05-18 23:27:13 +0300

    The core layer was deleted and replaced with an entity layer.

--- Portfolio.UI/Program.cs
+++ Portfolio.UI/Program.cs
@@ -1,21 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -25,5 +23,9 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+app.MapControllerRoute(
+    name: "area",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    ).WithStaticAssets();
 
 app.Run();

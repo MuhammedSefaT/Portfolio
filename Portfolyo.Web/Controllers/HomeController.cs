5b9fa9f5520ea3f62149730848c97bdde515ccfa
Muhammed Sefa TAYAZ <114636627+MuhammedSefaT@users.noreply.github.com>
2026-09-19 12:21:06 +0300

    feat: Site çok sayfalı yapıya geçirildi ve koyu footer eklendi.

--- Portfolyo.Web/Controllers/HomeController.cs
+++ Portfolyo.Web/Controllers/HomeController.cs
@@ -6,19 +6,30 @@ namespace Portfolyo.Web.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
-    {
-        return View();
-    }
+    private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+    public HomeController(ILogger<HomeController> logger) => _logger = logger;
+
+    public IActionResult Index() => View();
+
+    // Adres çubuğunda Türkçe ve kısa görünsün diye ayrı yollar tanımlı.
+    [HttpGet]
+    [Route("hakkimda")]
+    public IActionResult About() => View();
+
+    [HttpGet]
+    [Route("projelerim")]
+    public IActionResult Projects() => View();
+
+    [HttpGet]
+    [Route("iletisim")]
+    public IActionResult Contact() => View();
+
+    [HttpGet]
+    [Route("gizlilik")]
+    public IActionResult Privacy() => View();
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+        => View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 }
